5a6c81a7e8c1cd767f67b8f05e0009b15d47abc8
Eugene Martinez <eugenejrmartinez@gmail.com>
2025-04-23 00:25:17 +0800

    Expose WSGI application as 'app' for Vercel runtime

--- ghostnote_project/wsgi.py
+++ ghostnote_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ghostnote_project.settings')
 
 application = get_wsgi_application()
+
+# Add this line to expose the application object as 'app' for Vercel
+app = application
